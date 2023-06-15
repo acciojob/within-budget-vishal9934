27d01030e247ac24c89086b3267256cfd493df8f
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-06-15 11:25:26 +0530

    submitting code-Thu Jun 15 2023 11:25:26 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+let products=[
+  {name:'Apple',value:20},
+  {name:'Banana',value:10},
+  {name:'Neutella',value:30},
+  {name:'Razor',value:5},
+  {name:'CornFlakes',value:15},
+  {name:'Sound Bar',value:50},
+  {name:'Iphone',value:80}
+]
 const App = () => {
+  const [budget,setBudget]=useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <h3>Enter your budget to check available items:</h3>
+        <input value={budget} onChange={(e)=>setBudget(e.target.value)} type="number"/>
+        <table>
+
+        <thead><th>Items you can buy are in Green color</th></thead>
+        <tbody className="productsList">
+          {
+            products.map((product)=>{
+              return <tr className='product'>
+                  <td>{product.name}</td>
+                  <td style={{color:(product.value>budget)?"red":"green"}}>{product.value}</td>
+                </tr>
+            })
+          }
+        </tbody>
+        </table>
     </div>
   )
 }
